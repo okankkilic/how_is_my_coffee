198697d52c8ce6417a9b8872526b9c04fe53df27
okankkilic <okankkilic@gmail.com>
2024-03-02 23:47:22 +0300

    Update

--- lib/widgets/items.widget.dart
+++ lib/widgets/items.widget.dart
@@ -15,7 +15,7 @@ class ItemsWidget extends StatelessWidget {
         'Step 2: Steam 1/2 cup milk to 150 F.  Transfer the milk to a glass measuring cup and tap it on the counter a few times to break up any large bubbles.\n'
         'Step 3: Pour the steamed milk over the espresso, using a spoon to hold back the foam.\n'
         'Step 4: Top off the drink with the reserved foam. If you’re feeling fancy, you can use the foam to create latte art.\n',
-    '2',
+    'Espresso machine, AeroPress, Moka pot, French press, Instant espresso, Keurig',
     '3',
     '4',
     '5'
